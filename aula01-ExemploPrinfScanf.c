2878b3c49edd72c7043cb6ad464dd9ff41792fb1
Eli Jhonathan <106014593+elijhonathan@users.noreply.github.com>
2025-02-22 23:30:06 -0300

    Create aula01-ExemploPrinfScanf.c

---
+++ aula01-ExemploPrinfScanf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int x;
+	float y;
+	
+	printf("Entre com dois numeros: ");
+	scanf("%d %y", &x, &y);
+	
+	printf("Valor x: %d\nValor y: %f\n", x, y);
+	
+	return 0;
+}
